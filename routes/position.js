97e07cf26c26b70a3f2ea143d1d3abc5665f1a41
Your Name <yohai109@gmail.com>
2022-05-27 18:32:22 +0300

    add new route to get suggested users for a position

--- routes/position.js
+++ routes/position.js
@@ -6,119 +6,209 @@ Joi.ObjectId = require("joi-objectid")(Joi);
 const { Company } = require("../models/company");
 const { Position } = require("../models/position");
 const { validatePosition } = require("../models/position");
+const { User } = require("../models/user");
+const { max, minBy } = require("lodash");
 
 const router = express.Router();
 router.use(express.json());
 
 router.post(
-  "/:companyId",
-  auth,
-  asyncMiddleware(async (req, res) => {
-    const { error } = validatePosition(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+    "/:companyId",
+    auth,
+    asyncMiddleware(async (req, res) => {
+        const { error } = validatePosition(req.body);
+        if (error) return res.status(400).send(error.details[0].message);
 
-    let position = {
-      name: req.body.name,
-      description: req.body.description
-    };
+        let position = {
+            name: req.body.name,
+            description: req.body.description,
+        };
 
-    let length;
-    let company;
-    if (req.params.companyId) {
-      company = await Company.findById(req.params.companyId);
-      if (company) {
-        length = company.positions.push(position);
-        await company.save();
-      } else {
-        return res.status(404).send("The given company ID was not found.");
-      }
-    } else {
-      return res.status(400).send("Company ID is required.");
-    }
+        let length;
+        let company;
+        if (req.params.companyId) {
+            company = await Company.findById(req.params.companyId);
+            if (company) {
+                length = company.positions.push(position);
+                await company.save();
+            } else {
+                return res
+                    .status(404)
+                    .send("The given company ID was not found.");
+            }
+        } else {
+            return res.status(400).send("Company ID is required.");
+        }
 
-    position = company.positions[length - 1];
+        position = company.positions[length - 1];
 
-    res.send(position);
-  })
+        res.send(position);
+    })
 );
 
 router.get(
-  "/:companyId/:positionId",
-  auth,
-  asyncMiddleware(async (req, res) => {
-    let position;
-    const company = await Company.findById(req.params.companyId);
-    if (company && req.params.positionId && company.positions) {
-      position = company.positions.find(
-        position => position._id == req.params.positionId
-      );
-    }
-    return position
-      ? res.send(position)
-      : company && company.positions
-      ? res.status(404).send("The givan position ID is not found")
-      : res
-          .status(404)
-          .send("This company is not exist or it has no positions");
-  })
+    "/:companyId/:positionId",
+    auth,
+    asyncMiddleware(async (req, res) => {
+        let position;
+        const company = await Company.findById(req.params.companyId);
+        if (company && req.params.positionId && company.positions) {
+            position = company.positions.find(
+                (position) => position._id == req.params.positionId
+            );
+        }
+        return position
+            ? res.send(position)
+            : company && company.positions
+            ? res.status(404).send("The givan position ID is not found")
+            : res
+                  .status(404)
+                  .send("This company is not exist or it has no positions");
+    })
 );
 
 router.get(
-  "/:companyId",
-  auth,
-  asyncMiddleware(async (req, res) => {
-    const company = await Company.findById(req.params.companyId);
-    if (company && company.positions) {
-      return res.send(company.positions);
-    }
-    res.status(404).send("This company was not exist or it has no positions");
-  })
+    "/:companyId",
+    auth,
+    asyncMiddleware(async (req, res) => {
+        const company = await Company.findById(req.params.companyId);
+        if (company && company.positions) {
+            return res.send(company.positions);
+        }
+        res.status(404).send(
+            "This company was not exist or it has no positions"
+        );
+    })
 );
 
 router.delete(
-  "/:companyId/:positionId",
-  auth,
-  asyncMiddleware(async (req, res) => {
-    let position;
-    const company = await Company.findById(req.params.companyId);
-    if (company) {
-      position = company.positions.id(req.params.positionId);
-    } else {
-      res.status(404).send("The givan company ID was not found");
-    }
-    position.remove();
-    await company.save();
-    res.send(position);
-  })
+    "/:companyId/:positionId",
+    auth,
+    asyncMiddleware(async (req, res) => {
+        let position;
+        const company = await Company.findById(req.params.companyId);
+        if (company) {
+            position = company.positions.id(req.params.positionId);
+        } else {
+            res.status(404).send("The givan company ID was not found");
+        }
+        position.remove();
+        await company.save();
+        res.send(position);
+    })
 );
 
 router.put(
-  "/:companyId/:positionId",
-  auth,
-  asyncMiddleware(async (req, res) => {
-    const company = await Company.findById(req.params.companyId);
-    let position;
-    if (company) {
-      if (company.positions) {
-        position = company.positions.find(
-          position => position._id == req.params.positionId
-        );
-        if (position) {
-          position.name = req.body.name;
-          position.description = req.body.description;
+    "/:companyId/:positionId",
+    auth,
+    asyncMiddleware(async (req, res) => {
+        const company = await Company.findById(req.params.companyId);
+        let position;
+        if (company) {
+            if (company.positions) {
+                position = company.positions.find(
+                    (position) => position._id == req.params.positionId
+                );
+                if (position) {
+                    position.name = req.body.name;
+                    position.description = req.body.description;
 
-          await company.save();
-          res.send(position);
+                    await company.save();
+                    res.send(position);
+                } else {
+                    res.status(404).send("The givan position ID was not found");
+                }
+            } else {
+                res.status(404).send("This company was not have positions");
+            }
         } else {
-          res.status(404).send("The givan position ID was not found");
+            res.status(404).send("The givan company ID was not found");
         }
-      } else {
-        res.status(404).send("This company was not have positions");
-      }
-    } else {
-      res.status(404).send("The givan company ID was not found");
-    }
-  })
+    })
+);
+
+router.app.get(
+    "/suggestions/:companyId/:positionId",
+    auth,
+    asyncMiddleware(async (req, res) => {
+        const company = await Company.findById(req.params.companyId);
+        const count = parseInt(req.query.count) ? req.query.count : 10;
+
+        if (company) {
+            if (company.positions) {
+                const position = company.positions.find(
+                    (position) => position._id == req.params.positionId
+                );
+
+                if (position) {
+                    let suggestedUsers = [];
+                    const pageSize = 20;
+                    for (
+                        let page = 0;
+                        page * pageSize < (await User.count);
+                        page++
+                    ) {
+                        const users = User.find(
+                            {},
+                            {
+                                _id: 1,
+                                firstName: 1,
+                                lastName: 1,
+                                email: 1,
+                                cvs: 1,
+                            }
+                        )
+                            .skip(page * pageSize)
+                            .limit(pageSize);
+
+                        users.forEach((currUser) => {
+                            let userScore = 0;
+                            currUser.cvs.forEach((currCv) => {
+                                let cvScore = 0;
+                                currCv.tags.forEach((currTag) => {
+                                    if (position.tags.inclues(currTag)) {
+                                        cvScore++;
+                                    }
+                                });
+
+                                userScore = max(userScore, cvScore);
+                            });
+
+                            if (suggestedUsers.length < count) {
+                                suggestedUsers.push({
+                                    user: currUser,
+                                    score: userScore,
+                                });
+                            } else {
+                                const currMin = minBy(
+                                    suggestedUsers,
+                                    (currUserSuggestion) =>
+                                        currUserSuggestion.score
+                                );
+
+                                if (currMin < userScore) {
+                                    const minIndex = suggestedUsers.indexOf(
+                                        (curr) => curr.socre === currMin
+                                    );
+                                    suggestedUsers[minIndex] = {
+                                        user: currUser,
+                                        score: userScore,
+                                    };
+                                }
+                            }
+                        });
+                    }
+                    res.send(suggestedUsers);
+                } else {
+                    res.status(404).send("The givan position ID was not found");
+                }
+            } else {
+                res.status(404).send("This company was not have positions");
+            }
+        } else {
+            res.status(404).send("The givan company ID was not found");
+        }
+    })
 );
 
 module.exports = router;
